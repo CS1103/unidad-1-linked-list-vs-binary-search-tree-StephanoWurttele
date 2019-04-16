390095235c1d730b35812885942d6fc3829ed675
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-04-15 22:58:58 -0500

    Update Location.cpp

--- Location.cpp
+++ Location.cpp
@@ -4,4 +4,22 @@
 
 #include "Location.h"
 
+Location::Location(
+    int _position_id,
+    std::string _state_code,
+    std::string _county,
+    double _latitude,
+    double _longitude,
+    std::string _line,
+    std::string _construction):position_id(_position_id),
+    state_code(_state_code),
+    county(_county),
+    latitude(_latitude),
+    longitude(_longitude),
+    line(_line),
+    construction(_construction){};
+
+    int Location::getpos(){
+        return position_id;
+    };
 // Defina los metodos que crea necesario
