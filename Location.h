a6ca416f3a9d087b45590209e7ca84c2eb1d6564
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-04-15 22:59:21 -0500

    Update Location.h

--- Location.h
+++ Location.h
@@ -5,6 +5,9 @@
 #ifndef LINKEDLISTVSBINARYTREE_LOCATION_H
 #define LINKEDLISTVSBINARYTREE_LOCATION_H
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
 class Location {
     int position_id;
@@ -16,14 +19,14 @@ class Location {
     std::string construction;
 public:
     Location(
-            int position_id,
-            std::string state_code,
-            std::string county,
-            double latitude,
-            double longitude,
-            std::string line,
-            std::string construction);
-
+            int _position_id,
+            std::string _state_code,
+            std::string _county,
+            double _latitude,
+            double _longitude,
+            std::string _line,
+            std::string _construction);
+    int getpos();
     // Declare los metodos que crea necesario
 
     // Cree que sea necesario agregar destructor, justifique
